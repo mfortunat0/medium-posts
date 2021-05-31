6566f87dbae1c2e9c6668007fd1510499d919ed8
matheus <matheus.xmaz10@gmail.com>
2021-05-31 12:49:25 -0300

    Add babel html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: path.resolve(__dirname, "src", "index.js"),
   output: {
@@ -16,5 +16,8 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: "public/index.html"
+  })]
 };
